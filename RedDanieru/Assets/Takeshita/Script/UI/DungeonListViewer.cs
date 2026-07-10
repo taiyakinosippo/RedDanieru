f2ed18d4afc88cebeddf14cd6d5a8586ae14df8c
2459338 <172225910+2459338@users.noreply.github.com>
2026-07-10 15:28:41 +0900

    Reapply "ダンジョンマップ生成関連"
    This reverts commit 4343a3a4e8e1e5bb4f1d4ced4f400ee3e80588c6.

--- RedDanieru/Assets/Takeshita/Script/UI/DungeonListViewer.cs
+++ RedDanieru/Assets/Takeshita/Script/UI/DungeonListViewer.cs
@@ -1,35 +1,50 @@
+using System.Collections;
 using UnityEngine;
-using System.IO;
+using UnityEngine.Networking;
 
 public class DungeonListViewer : MonoBehaviour
 {
     public Transform content;
-
     public GameObject dungeonItemPrefab;
 
     public void RefreshList()
+    {
+        StartCoroutine(
+            RefreshListCoroutine()
+        );
+    }
+
+    private IEnumerator RefreshListCoroutine()
     {
         foreach (Transform child in content)
         {
             Destroy(child.gameObject);
         }
 
-        string[] files =
-            Directory.GetFiles(
-                Application.persistentDataPath,
-                "*.json"
+        UnityWebRequest request =
+            UnityWebRequest.Get(
+                "http://localhost/RedDaniel/list_dungeon.php"
             );
 
-        foreach(string file in files)
+        yield return request.SendWebRequest();
+
+        if (request.result !=
+            UnityWebRequest.Result.Success)
         {
-            string json =
-                File.ReadAllText(file);
+            Debug.LogError(request.error);
+            yield break;
+        }
 
-            DungeonData data =
-                JsonUtility.FromJson<DungeonData>(
-                    json
-                );
+        string json =
+            request.downloadHandler.text;
 
+        DungeonListResponse response =
+            JsonUtility.FromJson<DungeonListResponse>(
+                json
+            );
+
+        foreach (var dungeon in response.items)
+        {
             GameObject item =
                 Instantiate(
                     dungeonItemPrefab,
@@ -40,9 +55,9 @@ public class DungeonListViewer : MonoBehaviour
                 item.GetComponent<DungeonItemUI>();
 
             ui.Setup(
-                data.dungeonName,
-                data.creatorName,
-                data.createDate
+                dungeon.dungeon_name,
+                dungeon.creator_name,
+                dungeon.created_at
             );
         }
     }
